d7e295b6f552f4e8f6ef5f32f5c63bf4ce2e972e
Aliaksei <talerqa@gmail.com>
2025-05-02 00:11:41 +0300

    refactor: Refactor

--- src/hometask_01/routers/routers.ts
+++ src/hometask_01/routers/routers.ts
@@ -1,10 +1,41 @@
 import {Router} from 'express';
 import {HttpStatus} from "../../core/types/httpCodes";
+import {Video} from "../types/video";
 
 export const homeTask01Router = Router({});
 
+const db: Video[] = [{
+  id: 1,
+  title: 'Video 01',
+  author: 'Author 01',
+  canBeDownloaded: true,
+  minAgeRestriction: 18,
+  createdAt: '2025-05-01T12:00:00Z',
+  publicationDate: '2023-05-01T12:00:00Z',
+  availableResolutions: ['P240', "P720"]
+}, {
+  id: 2,
+  title: 'Video 02',
+  author: 'Author 02',
+  canBeDownloaded: true,
+  minAgeRestriction: 18,
+  createdAt: '2025-05-01T12:00:00Z',
+  publicationDate: '2023-05-01T12:00:00Z',
+  availableResolutions: ['P240', "P1080"]
+}, {
+  id: 3,
+  title: 'Video 03',
+  author: 'Author 03',
+  canBeDownloaded: true,
+  minAgeRestriction: 18,
+  createdAt: '2025-05-01T12:00:00Z',
+  publicationDate: '2023-05-01T12:00:00Z',
+  availableResolutions: ['P240', "P360"]
+}]
+
+
 homeTask01Router.get('/videos', (req, res) => {
-  res.status(200).send('hello world!!!');
+  res.status(200).send(db);
 });
 
 homeTask01Router.post('/videos', (req, res) => {
--- src/setup-app.ts
+++ src/setup-app.ts
@@ -5,10 +5,6 @@ import {setupSwagger} from "./core/swagger/setup-swagger";
 export const setupApp = (app: Express) => {
   app.use(express.json());
 
-  app.get('/', (req, res) => {
-    res.status(200).send('hello world!!!');
-  });
-
   app.use('/hometask_01/api', homeTask01Router);
   setupSwagger(app);
   return app;
